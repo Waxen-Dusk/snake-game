f77f5f33c8b10cc83a6ec1b55967e4ecf7cd59fc
Waxen-Dusk <mattdixondyer@gmail.com>
2025-01-29 21:41:29 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,108 +13,109 @@ let score = 0;
 let gameOverFlag = false;
 
 function generateFood() {
-    foodX = Math.floor(Math.random() * (gameWidth / snakeSize)) * snakeSize;
-    foodY = Math.floor(Math.random() * (gameHeight / snakeSize)) * snakeSize;
+  foodX = Math.floor(Math.random() * (gameWidth / snakeSize)) * snakeSize;
+  foodY = Math.floor(Math.random() * (gameHeight / snakeSize)) * snakeSize;
 }
 
 generateFood();
 
 function drawGame() {
-    ctx.clearRect(0, 0, gameWidth, gameHeight);
-    ctx.fillStyle = 'red';
-    ctx.fillRect(foodX, foodY, snakeSize, snakeSize);
-    ctx.fillStyle = 'green';
-    snake.forEach(segment => {
-        ctx.fillRect(segment.x, segment.y, snakeSize, snakeSize);
-        ctx.strokeRect(segment.x, segment.y, snakeSize, snakeSize);
-    });
+  ctx.clearRect(0, 0, gameWidth, gameHeight);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(foodX, foodY, snakeSize, snakeSize);
+  ctx.fillStyle = 'green';
+  snake.forEach(segment => {
+    ctx.fillRect(segment.x, segment.y, snakeSize, snakeSize);
+    ctx.strokeRect(segment.x, segment.y, snakeSize, snakeSize);
+  });
 }
 
 function checkCollision() {
-    if (snake.x < 0 || snake.x >= gameWidth || snake.y < 0 || snake.y >= gameHeight) {
-        gameOver();
-        return true;
-    }
+  if (snake.x < 0 || snake.x >= gameWidth || snake.y < 0 || snake.y >= gameHeight) {
+    gameOver();
+    return true;
+  }
 
-    for (let i = 1; i < snake.length; i++) {
-        if (snake.x === snake[i].x && snake.y === snake[i].y) {
-            gameOver();
-            return true;
-        }
+  for (let i = 1; i < snake.length; i++) {
+    if (snake.x === snake[i].x && snake.y === snake[i].y) {
+      gameOver();
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 function gameOver() {
-    clearInterval(gameLoopInterval);
-    alert('Game Over!');
-    gameOverFlag = true;
+  clearInterval(gameLoopInterval);
+  alert('Game Over!');
+  gameOverFlag = true;
 }
 
 function moveSnake() {
-    if (!gameOverFlag) {
-        const head = { x: snake.x + dx, y: snake.y + dy };
-        snake.unshift(head);
+  if (!gameOverFlag) {
+    const head = { x: snake.x + dx, y: snake.y + dy };
+    snake.unshift(head);
 
-        if (!checkCollision()) {
-            if (head.x === foodX && head.y === foodY) {
-                score++;
-                generateFood();
-            } else {
-                snake.pop();
-            }
+    if (!checkCollision()) {
+      if (head.x === foodX && head.y === foodY) {
+        score++;
+        generateFood();
+      } else {
+        snake.pop();
+      }
 
-            drawGame();
-        }
+      drawGame();
     }
+  }
 }
 
 function handleKeyPress(event) {
-    if (!gameOverFlag) {
-        const LEFT_KEY = 37;
-        const UP_KEY = 38;
-        const RIGHT_KEY = 39;
-        const DOWN_KEY = 40;
-        const keyPressed = event.keyCode;
+  if (!gameOverFlag) {
+    const LEFT_KEY = 37;
+    const UP_KEY = 38;
+    const RIGHT_KEY = 39;
+    const DOWN_KEY = 40;
+    const keyPressed = event.keyCode;
 
-        const goingUp = dy === -snakeSize;
-        const goingDown = dy === snakeSize;
-        const goingRight = dx === snakeSize;
-        const goingLeft = dx === -snakeSize;
+    const goingUp = dy === -snakeSize;
+    const goingDown = dy === snakeSize;
+    const goingRight = dx === snakeSize;
+    const goingLeft = dx === -snakeSize;
 
-        if (keyPressed === LEFT_KEY &&!goingRight) {
-            dx = -snakeSize;
-            dy = 0;
-        } else if (keyPressed === UP_KEY &&!goingDown) {
-            dx = 0;
-            dy = -snakeSize;
-        } else if (keyPressed === RIGHT_KEY &&!goingLeft) {
-            dx = snakeSize;
-            dy = 0;
-        } else if (keyPressed === DOWN_KEY &&!goingUp) {
-            dx = 0;
-            dy = snakeSize;
-        }
-    } else {
-        resetGame();
+    if (keyPressed === LEFT_KEY &&!goingRight) {
+      dx = -snakeSize;
+      dy = 0;
+    } else if (keyPressed === UP_KEY &&!goingDown) {
+      dx = 0;
+      dy = -snakeSize;
+    } else if (keyPressed === RIGHT_KEY &&!goingLeft) {
+      dx = snakeSize;
+      dy = 0;
+    } else if (keyPressed === DOWN_KEY &&!goingUp) {
+      dx = 0;
+      dy = snakeSize;
     }
+  } else {
+    resetGame();
+  }
 }
 
 function resetGame() {
-    clearInterval(gameLoopInterval);
-    snake = [{ x: gameWidth / 2, y: gameHeight / 2 }];
-    dx = snakeSize;
-    dy = 0;
-    score = 0;
-    gameOverFlag = false;
-    generateFood();
-    gameLoop();
+  clearInterval(gameLoopInterval);
+  snake = [{ x: gameWidth / 2, y: gameHeight / 2 }];
+  dx = snakeSize;
+  dy = 0;
+  score = 0;
+  gameOverFlag = false;
+  generateFood();
+  gameLoop();
 }
 
 let gameLoopInterval;
+
 function gameLoop() {
-    gameLoopInterval = setInterval(moveSnake, 100);
+  gameLoopInterval = setInterval(moveSnake, 100);
 }
 
 window.addEventListener('keydown', handleKeyPress);
